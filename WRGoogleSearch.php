bdc5eb75f41eeb6b84d6af51f795a8246c8417ff
Dror S. [ffs] <FreedomFighterSparrow@gmail.com>
2017-04-26 15:08:35 +0300

    Convert to MediaWiki's extension registration (extension.json)
    See https://www.mediawiki.org/wiki/Extension_registration

--- WRGoogleSearch.php
+++ WRGoogleSearch.php
@@ -1,90 +1,15 @@
 <?php
-/** \file
-* \brief Contains setup code for the WRGoogleSearch Extension.
-*/
-
-/**
- * WRGoogleSearch Extension for MediaWiki
- * Originally based on GoogleSiteSearch (v2.0) by Ryan Finnie
- *
- * Copyright (C) Dror S. & Kol-Zchut Ltd, Ryan Finnie
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
- * http://www.gnu.org/copyleft/gpl.html
- */
-
-# Not a valid entry point, skip unless MEDIAWIKI is defined
-if ( !defined( 'MEDIAWIKI' ) ) {
-	die( 1 );
+if ( function_exists( 'wfLoadExtension' ) ) {
+	wfLoadExtension( 'WikiRights/WRGoogleSearch' );
+	// Keep i18n globals so mergeMessageFileList.php doesn't break
+	$wgMessagesDirs['WRGoogleSearch'] = __DIR__ . '/i18n';
+	$wgExtensionMessagesFiles['WRGoogleSearchAlias'] = __DIR__ . '/WRGoogleSearch.alias.php';
+	wfWarn(
+		'Deprecated PHP entry point used for the WRGoogleSearch extension. ' .
+		'Please use wfLoadExtension instead, ' .
+		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+	);
+	return;
+} else {
+	die( 'This version of the WRGoogleSearch extension requires MediaWiki 1.25+' );
 }
-
-$wgExtensionCredits['specialpage'][] = [
-	'path' => __FILE__,
-	'name' => 'WRGoogleSearch',
-	'author' => [
-		'Dror S. [FFS] ([http://www.kolzchut.org.il Kol-Zchut])',
-		'Ryan Finnie'
-	],
-	'url' => 'https://www.mediawiki.org/wiki/Extension:GoogleSiteSearch',
-	'descriptionmsg' => 'wrgooglesearch-desc',
-	'version' => '1.1.0',
-];
-
-# Default configuration globals
-$wgWRGoogleSearchCSEID = '';
-$wgWRGoogleSearchOnly = false;
-$wgWRGoogleSearchExemptGroups = [];
-$wgWRGoogleSearchEnableSitelinksSearch = true;
-
-# Load classes
-$wgAutoloadClasses['SpecialWRGoogleSearch'] = __DIR__ . '/SpecialWRGoogleSearch.php';
-$wgAutoloadClasses['WRGoogleSearchHooks'] = __DIR__ . '/WRGoogleSearch.hooks.php';
-
-# Define special page
-$wgSpecialPages['WRGoogleSearch'] = 'SpecialWRGoogleSearch';
-
-# Define localization
-$wgMessagesDirs['WRGoogleSearch'] = __DIR__ . '/i18n';
-$wgExtensionMessagesFiles['WRGoogleSearchAlias'] = __DIR__  . '/WRGoogleSearch.alias.php';
-
-# Define hooks
-$wgHooks['BeforePageDisplay'][] = 'WRGoogleSearchHooks::onBeforePageDisplay';
-$wgHooks['ResourceLoaderGetConfigVars'][] = 'WRGoogleSearchHooks::onResourceLoaderGetConfigVars';
-$wgHooks['SkinAfterBottomScripts'][] = 'WRGoogleSearchHooks::onSkinAfterBottomScripts';
-
-# Define ResourceLoader js/css modules
-$modulesTemplate = [
-	'localBasePath' => __DIR__ . '/modules',
-	'remoteExtPath' => 'WikiRights/WRGoogleSearch/modules',
-];
-
-$wgResourceModules['ext.wrGoogleSearch.general'] = $modulesTemplate + [
-	'scripts' => 'ext.wrGoogleSearch.general.js',
-	'position' => 'bottom',
-	'dependencies' => 'mediawiki.util',
-];
-
-$wgResourceModules['ext.wrGoogleSearch.special'] = $modulesTemplate + [
-	'scripts' => 'ext.wrGoogleSearch.special.js',
-	'styles' => [
-		'ext.wrGoogleSearch.special.less',
-		'ext.wrGoogleSearch.results.css'
-	],
-	'position' => 'top',
-];
-
-unset( $modulesTemplate );
-
-
